7c0f1f16dc612fa13b095690b7c52fca3a2c2f34
lajos papp <lalyos@yahoo.com>
2014-09-26 21:57:32 +0000

    ignore bas cert: InsecureSkipVerify

--- src/ngrok/server/tls.go
+++ src/ngrok/server/tls.go
@@ -4,9 +4,13 @@ import (
 	"crypto/tls"
 	"io/ioutil"
 	"ngrok/server/assets"
+"fmt"
 )
 
 func LoadTLSConfig(crtPath string, keyPath string) (tlsConfig *tls.Config, err error) {
+
+fmt.Println("[DEBUG] crtPath=", crtPath, "keyPath=", keyPath)
+
 	fileOrAsset := func(path string, default_path string) ([]byte, error) {
 		loadFn := ioutil.ReadFile
 		if path == "" {
@@ -14,6 +18,7 @@ func LoadTLSConfig(crtPath string, keyPath string) (tlsConfig *tls.Config, err e
 			path = default_path
 		}
 
+fmt.Println("[HACK] 1")
 		return loadFn(path)
 	}
 
@@ -24,20 +29,25 @@ func LoadTLSConfig(crtPath string, keyPath string) (tlsConfig *tls.Config, err e
 	)
 
 	if crt, err = fileOrAsset(crtPath, "assets/server/tls/snakeoil.crt"); err != nil {
+fmt.Println("[HACK] 2")
 		return
 	}
 
 	if key, err = fileOrAsset(keyPath, "assets/server/tls/snakeoil.key"); err != nil {
+fmt.Println("[HACK] 3")
 		return
 	}
 
 	if cert, err = tls.X509KeyPair(crt, key); err != nil {
+fmt.Println("[HACK] i4")
 		return
 	}
 
 	tlsConfig = &tls.Config{
 		Certificates: []tls.Certificate{cert},
+		InsecureSkipVerify: true,
 	}
 
+fmt.Println("[HACK] 5")
 	return
 }
